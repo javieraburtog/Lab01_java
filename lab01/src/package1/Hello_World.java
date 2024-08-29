88a9bc1b08a481be93de882c62f10fa0fab7f212
Javier Aburto <jaburto@casapellas.com>
2024-08-28 20:22:21 -0600

    v2

--- lab01/src/package1/Hello_World.java
+++ lab01/src/package1/Hello_World.java
@@ -4,7 +4,9 @@ public class Hello_World {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello1");
+		System.out.println("Hello 1");
+		System.out.println("Hello 2");
+		
 
 	}
 
